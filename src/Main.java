757c88442166aec60986bfec8fb0f7ad0c3c8f56
shati <shatilova_ira@mail.ru>
2022-11-21 17:24:03 +0600

    3 Task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args) { task1(); task2 (); }
-    public static void task1 () {
+    public static void main(String[] args) { task1(); task2(); task3(); }
+    public static void task1() {
         System.out.println("Homework5");
         //Задание 1
         System.out.println(" ");
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-        public static void task2 () {
+    public static void task2() {
         //Задание 2
         System.out.println (" ");
         System.out.println ("Задание 2");
@@ -30,6 +30,20 @@ public class Main {
             } else {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
+        }
+    }
+    public static void task3() {
+        //Задание 3
+        System.out.println(" ");
+        System.out.println("Задание 3"); // вычислить является год високосным или нет
+        int year = 2021;
+        int year4 = year % 4;
+        int year100 = year % 100;
+        int year400 = year % 400;
+        if (year4 == 0 && year100 != 0 || year400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
         }
     }
 }
