589335fc0c8f0f274b0a20697dfad8451928821d
shati <shatilova_ira@mail.ru>
2022-11-21 15:26:20 +0600

    2 Task

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { task1(); task2 (); }
+    public static void task1 () {
         System.out.println("Homework5");
         //Задание 1
-        System.out.println (" ");
-        System.out.println ("Задание 1");
+        System.out.println(" ");
+        System.out.println("Задание 1");
         int clientOS = 1; // 0 - iOS, 1 - Android
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
@@ -11,6 +12,24 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-
-
+        public static void task2 () {
+        //Задание 2
+        System.out.println (" ");
+        System.out.println ("Задание 2");
+        int clientOS = 0; // 0 - iOS, 1 - Android
+        int clientDeviceYear = 2010; // < 2015 - облегченная версия, >= 2015 - обычная
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+    }
 }
