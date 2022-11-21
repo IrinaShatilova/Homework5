47e093d107a954bcfd23e0b875222abf36305d91
shati <shatilova_ira@mail.ru>
2022-11-21 14:19:07 +0600

    1 Task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework5");
+        //Задание 1
+        System.out.println (" ");
+        System.out.println ("Задание 1");
+        int clientOS = 1; // 0 - iOS, 1 - Android
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
+
+
 }
